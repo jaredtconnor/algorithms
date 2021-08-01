a9b5f0f7323c127a00e53d85aaa7c32574d03319
Jared Connor <jaredconnor301@gmail.com>
2021-08-01 13:58:21 -0700

    Adding shortest path algorithm

---
+++ Graph-shortest-distance.py
@@ -0,0 +1,34 @@
+import heapq
+import math
+
+def shortest_path(graph, starting_vertex): 
+    distances = {vertex: math.inf for vertex in graph}
+    distances[starting_vertex] = 0 
+
+    pq = [(0, starting_vertex)]
+
+    while len(pq) > 0: 
+        current_distance, current_vertex = heapq.heappop(pq) 
+
+        if current_distance > distances[current_vertex]: 
+            continue
+
+        for neighbor, weight, in graph[current_vertex.items()]: 
+            distance = current_distance + weight
+
+            if distance < distances[neighbor]: 
+                distances[neighbor] = distance
+                heapq.heappush(pq, (distance, neighbor))
+
+    return distances
+
+test_graph = {
+    'U': {'V': 2, 'W': 5, 'X': 1},
+    'V': {'U': 2, 'X': 2, 'W': 3},
+    'W': {'V': 3, 'U': 5, 'X': 3, 'Y': 1, 'Z': 5},
+    'X': {'U': 1, 'V': 2, 'W': 3, 'Y': 1},
+    'Y': {'X': 1, 'W': 1, 'Z': 1},
+    'Z': {'W': 5, 'Y': 1},
+}
+
+print(shortest_path(test_graph, 'Z'))
