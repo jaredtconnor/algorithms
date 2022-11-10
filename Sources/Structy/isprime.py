fd2da89fa339379281bb76503cdb49a2688914f6
Jared Connor <jaredconnor301@gmail.com>
2022-11-09 21:24:13 -0800

    Adding `isprime()`

--- Sources/Structy/isprime.py
+++ Sources/Structy/isprime.py
@@ -1,16 +1,53 @@
+import unittest  
+from math import sqrt, floor
+
+def isPrime_naive(num): 
+
+    if num < 2: 
+        return False
+
+    for i in range(2, num): 
+
+        if num % i == 0: 
+            return False   
+
+    return True
+
+def isPrime_sqrt(num):    
+
+    if num < 2: 
+        return False
+
+    for i in range(2, floor(sqrt(num) + 1)): 
+
+        if num % i == 0: 
+            return False
+
+    return True
+
+class Test(unittest.TestCase):
+
+    test_cases = { 
+        (2, True), 
+        (3, True), 
+        (4, False),
+        (5, True), 
+        (6, False),  
+        (31, True), 
+        (2017, True), 
+        (2048, False)
+    }
+
+    def test_1(self): 
+
+        for num, expected in self.test_cases:   
+            assert isPrime_naive(num) == expected 
+
+    def test_2(self): 
+        
+        for num, expected in self.test_cases: 
+            assert isPrime_sqrt(num) == expected
 
 
-def isPrime(nums):
-
-    for i in range(1, nums): 
-
-        if nums % i == 0: 
-            return False 
-
-
-
-
-
-
-
-
+if __name__ == "__main__":
+    unittest.main()
