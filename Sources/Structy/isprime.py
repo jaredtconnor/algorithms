22f349410d0bec5378c7878d2d69a0c546dbef49
Jared Connor <jaredconnor301@gmail.com>
2022-11-10 05:29:40 +0000

    Adding isprime()

--- Sources/Structy/isprime.py
+++ Sources/Structy/isprime.py
@@ -1,51 +1,54 @@
-import unittest  
+import unittest
 from math import sqrt, floor
 
-def isPrime_naive(num): 
 
-    if num < 2: 
+def isPrime_naive(num):
+
+    if num < 2:
         return False
 
-    for i in range(2, num): 
+    for i in range(2, num):
 
-        if num % i == 0: 
-            return False   
-
-    return True
-
-def isPrime_sqrt(num):    
-
-    if num < 2: 
-        return False
-
-    for i in range(2, floor(sqrt(num) + 1)): 
-
-        if num % i == 0: 
+        if num % i == 0:
             return False
 
     return True
 
+
+def isPrime_sqrt(num):
+
+    if num < 2:
+        return False
+
+    for i in range(2, floor(sqrt(num) + 1)):
+
+        if num % i == 0:
+            return False
+
+    return True
+
+
 class Test(unittest.TestCase):
 
-    test_cases = { 
-        (2, True), 
-        (3, True), 
+    test_cases = {
+        (2, True),
+        (3, True),
         (4, False),
-        (5, True), 
-        (6, False),  
-        (31, True), 
-        (2017, True), 
-        (2048, False)
+        (5, True),
+        (6, False),
+        (31, True),
+        (2017, True),
+        (2048, False),
     }
 
-    def test_1(self): 
+    def test_1(self):
 
-        for num, expected in self.test_cases:   
-            assert isPrime_naive(num) == expected 
+        for num, expected in self.test_cases:
+            assert isPrime_naive(num) == expected
 
-    def test_2(self): 
-        
-        for num, expected in self.test_cases: 
+    def test_2(self):
+
+        for num, expected in self.test_cases:
             assert isPrime_sqrt(num) == expected
 
 
