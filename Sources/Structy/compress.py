e3c307aff842b9e00739ff3ef0b4cbaa1b511cc7
Jared Connor <jaredconnor301@gmail.com>
2022-11-19 12:36:39 -0800

    Updating to reformat

--- Sources/Structy/compress.py
+++ Sources/Structy/compress.py
@@ -2,19 +2,19 @@ import unittest
 
 
 def compress(s):
-    
-    s += '!'
-    res = [] 
+
+    s += "!"
+    res = []
     i = 0
     j = 0
 
-    while j < len(s):  
-        
-        if s[i] == s[j]: # Records the streaks in string
-            j+=1 
+    while j < len(s):
 
-        else: 
-            num =  j - i  
+        if s[i] == s[j]:  # Records the streaks in string
+            j += 1
+
+        else:
+            num = j - i
 
             if num == 1:  # Single occurance of character
                 res.append(s[i])
@@ -22,25 +22,28 @@ def compress(s):
                 res.append(str(num))
                 res.append(s[i])
 
-            i = j 
+            i = j
 
-    return ''.join(res) 
+    return "".join(res)
 
 
 class Test(unittest.TestCase):
 
-    test_cases = {  
-        ('ccaaatsss', '2c3at3s'),
-        ('ssssbbz', '4s2bz'), 
-        ('ppoppppp', '2po5p'),  
-        ('nnneeeeeeeeeeeezz', '3n12e2z'),
-        ('yyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy', "127y"), 
+    test_cases = {
+        ("ccaaatsss", "2c3at3s"),
+        ("ssssbbz", "4s2bz"),
+        ("ppoppppp", "2po5p"),
+        ("nnneeeeeeeeeeeezz", "3n12e2z"),
+        (
+            "yyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy",
+            "127y",
+        ),
     }
 
     def test_1(self):
 
-        for s, expected in self.test_cases:  
-            #print(f"Input: {s} | Output: {compress(s)} | Expected: {expected} | Result {compress(s) == expected}")
+        for s, expected in self.test_cases:
+            # print(f"Input: {s} | Output: {compress(s)} | Expected: {expected} | Result {compress(s) == expected}")
             assert compress(s) == expected
 
 
