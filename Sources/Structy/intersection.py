3294694d8d5af02378cdb8d9c7df222eb8fb6d35
Jared Connor <jaredconnor301@gmail.com>
2022-11-27 22:04:40 +0000

    Adding `intersection()`

---
+++ Sources/Structy/intersection.py
@@ -0,0 +1,25 @@
+import unittest
+
+
+def intersection(list1, list2):
+
+
+    return
+
+
+class Test(unittest.TestCase):
+
+    test_cases = {
+
+
+    }
+
+    def test_1(self):
+
+        for s, expected in self.test_cases:
+            # print(f"Input: {s} | Output: {compress(s)} | Expected: {expected} | Result {compress(s) == expected}")
+            assert compress(s) == expected
+
+
+if __name__ == "__main__":
+    unittest.main()
