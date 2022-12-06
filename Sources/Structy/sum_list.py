5d190afc4f2e089cd09a76b0dbf2d8917270b558
Jared Connor <jaredconnor301@gmail.com>
2022-12-06 04:20:41 +0000

    Adding recursive solution

--- Sources/Structy/sum_list.py
+++ Sources/Structy/sum_list.py
@@ -20,6 +20,15 @@ def sum_list(head):
     return sum
 
 
+def sum_list_recurse(head):
+
+    # base case
+    if head is None:
+        return 0
+
+    return head.val + sum_list_recurse(head.next)
+
+
 class Test(unittest.TestCase):
     def test_1(self):
 
@@ -37,6 +46,23 @@ class Test(unittest.TestCase):
 
     def test_3(self):
         self.assertEqual(sum_list(None), 0)
+
+    def test_4(self):
+
+        x = Node(38)
+        y = Node(4)
+
+        x.next = y
+
+        self.assertEqual(sum_list_recurse(x), 42)
+
+    def test_5(self):
+        z = Node(100)
+
+        self.assertEqual(sum_list_recurse(z), 100)
+
+    def test_6(self):
+        self.assertEqual(sum_list_recurse(None), 0)
 
 
 if __name__ == "__main__":
