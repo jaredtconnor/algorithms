0864bd3caae5de839bcfe1b5249dc2d9ce87c7c6
Jared Connor <jaredconnor301@gmail.com>
2022-12-10 22:45:33 +0000

    Updating solution

--- Sources/Structy/linked_list_zipper_list.py
+++ Sources/Structy/linked_list_zipper_list.py
@@ -9,7 +9,32 @@ class Node:
 
 def zipper_list(head_a: Node, head_b: Node):
 
-    return
+    main_ctr = 0
+    current1, current2 = head_a.next, head_b
+    tail = head_a
+
+    while current1 is not None and current2 is not None:
+
+        if main_ctr % 2 == 0:  # even
+
+            tail.next = current2
+            current2 = current2.next
+
+        else:
+
+            tail.next = current1
+            current1 = current1.next
+
+        tail = tail.next
+        main_ctr += 1  # odd
+
+    if current1 is not None:
+        tail.next = current1
+
+    if current2 is not None:
+        tail.next = current2
+
+    return head_a
 
 
 class Test(unittest.TestCase):
