445afbf5072150f053d3741a25f0f31ae48039eb
Jared Connor <jaredconnor301@gmail.com>
2022-12-13 02:50:22 +0000

    Adding recursive solution

--- Sources/Structy/linked_list_zipper_list.py
+++ Sources/Structy/linked_list_zipper_list.py
@@ -9,25 +9,25 @@ class Node:
 
 def zipper_list(head_a: Node, head_b: Node):
 
-    main_ctr = 0
-    current1, current2 = head_a.next, head_b
     tail = head_a
+    current1, current2 = head_a.next, head_b
+    count = 0
 
     while current1 is not None and current2 is not None:
 
-        if main_ctr % 2 == 0:  # even
+        if count % 2 == 0:
 
             tail.next = current2
             current2 = current2.next
 
         else:
-
             tail.next = current1
             current1 = current1.next
 
         tail = tail.next
-        main_ctr += 1  # odd
+        count += 1
 
+    # section to carry over if one list is null
     if current1 is not None:
         tail.next = current1
 
@@ -35,6 +35,41 @@ def zipper_list(head_a: Node, head_b: Node):
         tail.next = current2
 
     return head_a
+
+
+def zipper_list_recursive(head_a: Node, head_b: Node) -> Node:
+
+    if head_a is None and head_b is None:
+
+        return None
+
+    if head_a is None:
+        return head_b
+
+    if head_b is None:
+        return head_a
+
+    next_1 = head_a.next
+    next_2 = head_b.next
+
+    head_a.next = head_b
+    head_b.next = zipper_list_recursive(next_1, next_2)
+
+    return head_a
+
+
+def print_list(head):
+
+    res = ""
+    current = head
+
+    while current.next is not None:
+        res += current.val + " -> "
+        current = current.next
+
+    res += current.val
+
+    return res
 
 
 class Test(unittest.TestCase):
@@ -54,8 +89,29 @@ class Test(unittest.TestCase):
         y.next = z
         # x -> y -> z
 
-        zipper_lists(a, x)
-        # a -> x -> b -> y -> c -> z`
+        expected = "a -> x -> b -> y -> c -> z"
+        res = zipper_list(a, x)
+        assert print_list(res) == expected
+
+    def test_2(self):
+        a = Node("a")
+        b = Node("b")
+        c = Node("c")
+
+        a.next = b
+        b.next = c
+        # a -> b -> c
+
+        x = Node("x")
+        y = Node("y")
+        z = Node("z")
+        x.next = y
+        y.next = z
+        # x -> y -> z
+
+        expected = "a -> x -> b -> y -> c -> z"
+        res = zipper_list_recursive(a, x)
+        assert print_list(res) == expected
 
 
 if __name__ == "__main__":
