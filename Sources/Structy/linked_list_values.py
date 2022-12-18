3fec5246154872ea3caf0b60a2cff4b8bde22512
Jared Connor <jaredconnor301@gmail.com>
2022-12-18 11:49:32 -0800

    Updating to include recursive case

--- Sources/Structy/linked_list_values.py
+++ Sources/Structy/linked_list_values.py
@@ -20,7 +20,38 @@ def linked_list_values(head):
     return vals
 
 
+def linked_list_values_recurse(head):
+
+    vals = []
+    _linked_list_values_fill_values(head, vals)
+    return vals
+
+
+def _linked_list_values_fill_values(head, vals):
+
+    if head is None:
+        return
+
+    vals.append(head.val)
+    _linked_list_values_fill_values(head.next, vals)
+
+
+def print_list(head):
+
+    res = ""
+    current = head
+
+    while current.next is not None:
+        res += current.val + " -> "
+        current = current.next
+
+    res += current.val
+
+    return res
+
+
 class Test(unittest.TestCase):
+
     def test_1(self):
 
         a = Node("a")
@@ -32,7 +63,25 @@ class Test(unittest.TestCase):
         b.next = c
         c.next = d
 
-        linked_list_values(a)
+        expected = ["a", "b", "c", "d"]
+        res = linked_list_values(a)
+        assert res == expected
+
+
+    def test_2(self):
+
+        a = Node("a")
+        b = Node("b")
+        c = Node("c")
+        d = Node("d")
+
+        a.next = b
+        b.next = c
+        c.next = d
+
+        expected = ["a", "b", "c", "d"]
+        res = linked_list_values_recurse(a)
+        assert res == expected
 
 
 if __name__ == "__main__":
