5d95d64b633a8f9f6b9a68b26a227d9f5ee5c76a
Jared Connor <jaredconnor301@gmail.com>
2022-12-26 23:09:55 +0000

    Adding recursive solution to `linked_list_create()`

--- Sources/Structy/linked_list_create.py
+++ Sources/Structy/linked_list_create.py
@@ -20,6 +20,19 @@ def create_linked_list(input: list):
     return dummy.next
 
 
+def create_linked_list_recursive(input: list):
+
+    # Base Case - if input is end
+    if len(input) == 0:
+        return None
+
+    head = Node(str(input[0]))
+
+    head.next = create_linked_list_recursive(input[1:])
+
+    return head
+
+
 def print_list(head):
 
     res = ""
@@ -59,6 +72,34 @@ class Test(unittest.TestCase):
     def test_4(self):
 
         res = create_linked_list([])
+        expected = None
+
+        assert res == expected
+
+    def test_5(self):
+
+        res = create_linked_list_recursive(["h", "e", "y"])
+        expected = "h -> e -> y"
+
+        assert print_list(res) == expected
+
+    def test_6(self):
+
+        res = create_linked_list_recursive([1, 7, 1, 8])
+        expected = "1 -> 7 -> 1 -> 8"
+
+        assert print_list(res) == expected
+
+    def test_7(self):
+
+        res = create_linked_list_recursive(["a"])
+        expected = "a"
+
+        assert print_list(res) == expected
+
+    def test_8(self):
+
+        res = create_linked_list_recursive([])
         expected = None
 
         assert res == expected
