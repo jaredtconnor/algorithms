6823363b27b3c840acd0d7c3c778824e1490d2bd
Jared Connor <jaredconnor301@gmail.com>
2022-12-27 20:45:51 -0800

    Initial binary tree bfs implementation

---
+++ Sources/Structy/bt-depth-first-values.py
@@ -0,0 +1,40 @@
+import unittest
+
+
+class Node:
+    def __init__(self, val):
+        self.val = val 
+        self.left = None
+        self.right = None
+
+
+
+
+def dbs_traversal(root: Node):  
+
+
+   
+    if not root:  
+        return []
+
+    stack = [root] 
+    values = []
+
+
+    while stack: 
+
+
+
+
+    return
+
+
+
+
+
+class Test(unittest.TestCase):
+    def test_1(self):
+        return
+
+if __name__ == "__main__":
+    unittest.main()
