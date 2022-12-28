7aff1daf897b574c5b6aa64d7522adc0db581c60
Jared Connor <jaredconnor301@gmail.com>
2022-12-27 20:47:10 -0800

    Updating inital bfs

--- Sources/Structy/bt-depth-first-values.py
+++ Sources/Structy/bt-depth-first-values.py
@@ -3,38 +3,36 @@ import unittest
 
 class Node:
     def __init__(self, val):
-        self.val = val 
+        self.val = val
         self.left = None
         self.right = None
 
 
+def dbs_traversal(root: Node):
 
-
-def dbs_traversal(root: Node):  
-
-
-   
-    if not root:  
+    if not root:
         return []
 
-    stack = [root] 
+    stack = [root]
     values = []
 
+    while stack:
 
-    while stack: 
+        node = stack.pop()
+        values.appened(node.val)
 
+        if node.right:
+            stack.append(node.right)
+        if node.left:
+            stack.append(node.left)
 
-
-
-    return
-
-
-
+    return values
 
 
 class Test(unittest.TestCase):
     def test_1(self):
         return
+
 
 if __name__ == "__main__":
     unittest.main()
