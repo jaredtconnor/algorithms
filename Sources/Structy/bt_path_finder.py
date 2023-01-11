6de3547b9fec5294e45df612d2ad875950c3108c
Jared Connor <jaredconnor301@gmail.com>
2023-01-11 05:07:54 -0800

    Adding spec out of `path_finder()`

---
+++ Sources/Structy/bt_path_finder.py
@@ -0,0 +1,44 @@
+import unittest
+from collections import deque
+
+
+class Node:
+    def __init__(self, val):
+        self.val = val
+        self.left = None
+        self.right = None
+
+
+def path_finder(root: Node, target: str):
+
+    return root.val
+
+
+class Test(unittest.TestCase):
+    def test_case_1(self):
+
+        assert res == expected
+
+    def test_case_2(self):
+
+        assert res == expected
+
+    def test_case_3(self):
+
+        assert res == expected
+
+    def test_case_4(self):
+
+        assert res == expected
+
+    def test_case_5(self):
+
+        assert res == expected
+
+    def test_case_6(self):
+
+        assert res == expected
+
+
+if __name__ == "__main__":
+    unittest.main()
