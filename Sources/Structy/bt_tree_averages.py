c77051ab8c8b4ad88c07ce97f8dcf6c6ad1e978f
Jared Connor <jaredconnor301@gmail.com>
2023-01-26 14:14:35 -0700

    Adding depth first search

--- Sources/Structy/bt_tree_averages.py
+++ Sources/Structy/bt_tree_averages.py
@@ -39,6 +39,32 @@ def level_averages(root: Node):
     return avgs
 
 
+def tree_averages_bfs(root: Node):
+
+    if root is None:
+        return []
+
+    levels = []
+    queue = deque([(root, 0)])
+
+    while queue:
+
+        current, level_num = queue.popleft()
+
+        if len(levels) == level_num:
+            levels.append([current.val])
+        else:
+            levels[level_num].append(current.val)
+
+        if current.left is not None:
+            queue.append((current.left, level_num + 1))
+
+        if current.right is not None:
+            queue.append((current.right, level_num + 1))
+
+    return levels
+
+
 class Test(unittest.TestCase):
     def test_case_1(self):
         a = Node(3)
