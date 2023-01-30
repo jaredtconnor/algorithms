5dc2f887fc85dd6950e9de209992580b2d8e1c01
Jared Connor <jaredconnor301@gmail.com>
2023-01-30 01:11:53 +0000

    Adding bfs for `tree_averages`

--- Sources/Structy/bt_tree_averages.py
+++ Sources/Structy/bt_tree_averages.py
@@ -1,4 +1,5 @@
 import unittest
+from collections import deque
 
 
 class Node:
@@ -39,7 +40,7 @@ def level_averages(root: Node):
     return avgs
 
 
-def tree_averages_bfs(root: Node):
+def level_averages_bfs(root: Node):
 
     if root is None:
         return []
@@ -61,6 +62,11 @@ def tree_averages_bfs(root: Node):
 
         if current.right is not None:
             queue.append((current.right, level_num + 1))
+
+    avgs = []
+
+    for level in levels:
+        avgs.append(sum(level) / len(level))
 
     return levels
 
@@ -180,6 +186,123 @@ class Test(unittest.TestCase):
     def test_case_5(self):
 
         res = level_averages(None)
+        expected = []
+        assert res == expected
+
+    def test_case_6(self):
+        a = Node(3)
+        b = Node(11)
+        c = Node(4)
+        d = Node(4)
+        e = Node(-2)
+        f = Node(1)
+
+        a.left = b
+        a.right = c
+        b.left = d
+        b.right = e
+        c.right = f
+
+        #       3
+        #    /    \
+        #   11     4
+        #  / \      \
+        # 4   -2     1
+
+        res = level_averages_bfs(a)
+        expected = [3, 7.5, 1]
+        assert res == expected
+
+    def test_case_2(self):
+        a = Node(5)
+        b = Node(11)
+        c = Node(54)
+        d = Node(20)
+        e = Node(15)
+        f = Node(1)
+        g = Node(3)
+
+        a.left = b
+        a.right = c
+        b.left = d
+        b.right = e
+        e.left = f
+        e.right = g
+
+        #        5
+        #     /    \
+        #    11    54
+        #  /   \
+        # 20   15
+        #      / \
+        #     1  3
+
+        res = level_averages_bfs(a)
+        expected = [5, 32.5, 17.5, 2]
+        assert res == expected
+
+    def test_case_3(self):
+        a = Node(-1)
+        b = Node(-6)
+        c = Node(-5)
+        d = Node(-3)
+        e = Node(0)
+        f = Node(45)
+        g = Node(-1)
+        h = Node(-2)
+
+        a.left = b
+        a.right = c
+        b.left = d
+        b.right = e
+        c.right = f
+        e.left = g
+        f.right = h
+
+        #        -1
+        #      /   \
+        #    -6    -5
+        #   /  \     \
+        # -3   0     45
+        #     /       \
+        #    -1       -2
+
+        res = level_averages_bfs(a)
+        expected = [-1, -5.5, 14, -1.5]
+        assert res == expected
+
+    def test_case_4(self):
+
+        q = Node(13)
+        r = Node(4)
+        s = Node(2)
+        t = Node(9)
+        u = Node(2)
+        v = Node(42)
+
+        q.left = r
+        q.right = s
+        r.right = t
+        t.left = u
+        u.right = v
+
+        #        13
+        #      /   \
+        #     4     2
+        #      \
+        #       9
+        #      /
+        #     2
+        #    /
+        #   42
+
+        res = level_averages_bfs(q)
+        expected = [13, 3, 9, 2, 42]
+        assert res == expected
+
+    def test_case_5(self):
+
+        res = level_averages_bfs(None)
         expected = []
         assert res == expected
 
