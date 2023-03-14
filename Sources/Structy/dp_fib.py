bcef68c822e5e39e6fef2d3b7aea71385bceac56
Jared Connor <jaredconnor301@gmail.com>
2023-03-14 02:33:37 +0000

    Update to `fib` formatting

--- Sources/Structy/dp_fib.py
+++ Sources/Structy/dp_fib.py
@@ -1,23 +1,25 @@
 import unittest
 
 
-def fib(n): 
+def fib(n):
 
     memo = {}
 
     return _fib(n, memo)
 
-def _fib(n, memo): 
 
-    if n == 0 or n == 1: 
+def _fib(n, memo):
+
+    if n == 0 or n == 1:
         return n
 
     if n in memo:
-        return memo[n] 
-    
-    memo[n] = _fib(n-1, memo) + _fib(n-2, memo)
+        return memo[n]
+
+    memo[n] = _fib(n - 1, memo) + _fib(n - 2, memo)
 
     return memo[n]
+
 
 class Test(unittest.TestCase):
     def test_case_1(self):
