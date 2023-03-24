c36d81c109c040d49c377e969c67f64e2b5963fd
Jared Connor <jaredconnor301@gmail.com>
2023-03-24 03:36:42 +0000

    Adding spec out of `min_change`

---
+++ Sources/Structy/dp_min_change.py
@@ -0,0 +1,18 @@
+import unittest
+
+
+def min_change(amount, coints):
+
+    return
+
+
+class Test(unittest.TestCase):
+    def test_case_1(self):
+
+        res = min_change(0)
+        expected = 0
+        assert res == expected
+
+
+if __name__ == "__main__":
+    unittest.main()
