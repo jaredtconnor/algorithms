1c381133b23aea9aca8645d15c1bf9eb4cc33911
Jared Connor <jaredconnor301@gmail.com>
2023-05-04 01:07:25 +0000

    Update to min_change

--- Sources/Structy/dp_min_change.py
+++ Sources/Structy/dp_min_change.py
@@ -2,7 +2,6 @@ import unittest
 
 
 def min_change(amount, coins):
-
     memo = {}
 
     min_coins = _min_change(amount, coins, memo)
@@ -14,7 +13,6 @@ def min_change(amount, coins):
 
 
 def _min_change(amount, coins, memo):
-
     if amount in memo:
         return memo[amount]
 
@@ -29,7 +27,6 @@ def _min_change(amount, coins, memo):
     min_coins = float("inf")
 
     for coin in coins:
-
         num_coins = 1 + _min_change(amount - coin, coins, memo)
 
         if num_coins < min_coins:
@@ -41,55 +38,46 @@ def _min_change(amount, coins, memo):
 
 class Test(unittest.TestCase):
     def test_case_1(self):
-
         res = min_change(8, [1, 5, 4, 12])  # -> 2
         expected = 2
         assert res == expected
 
     def test_case_2(self):
-
         expected = min_change(13, [1, 9, 5, 14, 30])  # -> 5
         res = 5
         assert res == expected
 
     def test_case_3(self):
-
         expected = min_change(23, [2, 5, 7])  # -> 4
         res = 4
         assert res == expected
 
     def test_case_4(self):
-
         expected = min_change(102, [1, 5, 10, 25])  # -> 6
         res = 6
         assert res == expected
 
     def test_case_5(self):
-
         expected = min_change(200, [1, 5, 10, 25])  # -> 8
         res = 8
         assert res == expected
 
     def test_case_6(self):
-
         expected = min_change(2017, [4, 2, 10])  # -> -1
         res = -1
         assert res == expected
 
     def test_case_7(self):
-
         expected = min_change(271, [10, 8, 265, 24])  # -> -1
         res = -1
         assert res == expected
 
     def test_case_8(self):
-
         expected = min_change(0, [4, 2, 10])  # -> 0
         res = 0
         assert res == expected
 
     def test_case_9(self):
-
         expected = min_change(0, [])  # -> 0
         res = 0
         assert res == expected
