d5abde81918728dd88ce81009ba0dc7dc85ff976
Jared Connor <jaredconnor301@gmail.com>
2023-07-25 09:16:06 -0700

    Adding spec of solution

--- Sources/Structy/dp_summing_squares.py
+++ Sources/Structy/dp_summing_squares.py
@@ -1,7 +1,34 @@
 import unittest
+import math
 
 def summing_squares(n):
-    return None
+    
+    memo = {}
+    return _summing_squares(n, memo)
+
+def _summing_squares(n, memo):
+    
+    # DP Memo check
+    if n in memo:
+        return memo[n]
+
+    # Base case
+    if n == 0:
+        return 0
+    
+    # Set really large at init
+    min_squares = float('inf')
+
+    for i in range(1, math.floor(math.sqrt(n)) + 1):
+        square = i * i
+
+        # Returns the min_squares required to reach
+        num_squares = 1 + _summing_squares(n - square, memo)
+
+        min_squares = min(num_squares, min_squares)
+
+    memo[n] = min_squares
+    return memo[n]
 
 
 class Test(unittest.TestCase):
@@ -37,7 +64,7 @@ class Test(unittest.TestCase):
 
     def test_case_7(self):
         res = summing_squares(87)
-        exp = 24
+        exp = 4
         assert res == exp
 
 
