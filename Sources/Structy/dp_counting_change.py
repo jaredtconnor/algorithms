367d806e573fe8a9340869144f01b2fcee491779
Jared Connor <jaredconnor301@gmail.com>
2023-08-01 11:57:39 -0700

    Final spec of `counting_change`

--- Sources/Structy/dp_counting_change.py
+++ Sources/Structy/dp_counting_change.py
@@ -2,7 +2,31 @@ import unittest
 
 
 def counting_change(amount, coins):
-    return None
+    memo = {}
+    return _counting_change(amount, coins, 0, memo)
+
+
+def _counting_change(amount, coins, i, memo):
+    key = (amount, i)
+
+    if key in memo:
+        return memo[key]
+
+    if amount == 0:
+        return 1
+
+    if i == len(coins):
+        return 0
+
+    coin = coins[i]
+
+    total_ways = 0
+    for qty in range(0, (amount // coin) + 1):
+        remainder = amount - (qty * coin)
+        total_ways += _counting_change(remainder, coins, i + 1, memo)
+
+    memo[key] = total_ways
+    return memo[key]
 
 
 class Test(unittest.TestCase):
