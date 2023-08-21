80c8c0fa93caf831b72e7710b06b1923867a921f
Jared Connor <jaredconnor301@gmail.com>
2023-08-21 16:05:18 -0700

    Adding array_stepper solution

--- Sources/Structy/dp_array_stepper.py
+++ Sources/Structy/dp_array_stepper.py
@@ -2,7 +2,27 @@ import unittest
 
 
 def array_stepper(numbers):
-    return False
+    memo = {}
+
+    return _array_stepper(numbers, 0, memo)
+
+
+def _array_stepper(numbers, i, memo):
+    if i in memo:
+        return memo[i]
+
+    if i >= len(numbers) - 1:
+        return True
+
+    max_step = numbers[i]
+
+    for step in range(1, max_step + 1):
+        if _array_stepper(numbers, i + step, memo):
+            memo[i] = True
+            return memo[i]
+
+    memo[i] = False
+    return memo[i]
 
 
 class Test(unittest.TestCase):
